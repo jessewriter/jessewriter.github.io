c8baea43c6732de9dbada79aed403f0be51298b6
jesse writer <jesseboydconsulting@gmail.com>
2025-07-25 10:34:26 -0700

    update from intelij

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/jessewriter.github.io/' // Replace with your repository name
-});
