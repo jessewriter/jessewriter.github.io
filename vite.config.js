9b31d2308e7718be0524b35e16bb5b50f91e2222
jesse writer <jesseboydconsulting@gmail.com>
2025-07-25 11:25:42 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/jessewriter.github.io/' // Replace with your repository name
+  base: '/jessewriter.github.io/', // Replace with your repository name
+  build: { outDir: 'docs'}
 });
